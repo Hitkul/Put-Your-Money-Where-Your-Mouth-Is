5839c0ba02fbe199685250c24fb9f32846b12bf8
hitkul <hitkuljangid@gmail.com>
2023-04-14 16:04:43 +0530

    main scraper file

---
+++ scraper/main.py
@@ -0,0 +1,25 @@
+import json
+from tqdm import tqdm
+import time 
+import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+################ Logging Setup ##################
+logger = logging.getLogger(f"Stickk data collector - {__name__}")
+logger.setLevel(logging.DEBUG)
+
+LOG_FILE  = "../logs/Stickk_data_Collector.log"
+c_handler = logging.StreamHandler()
+f_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
+
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
+###############################################
+
